c2927caf048b2258612a438be5dad89343552d32
Derek Allard <derek.allard@ellislab.com>
2009-11-12 14:58:20 -0500

    removing old logs

--- bamboo_system_files/logs/log-2009-10-30.php
+++
@@ -1,4 +0,0 @@
-<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
-
-ERROR - 2009-10-30 19:36:26 --> Severity: Warning  --> fsockopen() [<a href='function.fsockopen'>function.fsockopen</a>]: php_network_getaddresses: getaddrinfo failed: nodename nor servname provided, or not known /Users/derekallard/Sites/bambooinvoice/bamboo_system_files/application/models/utilities_model.php 12
-ERROR - 2009-10-30 19:36:26 --> Severity: Warning  --> fsockopen() [<a href='function.fsockopen'>function.fsockopen</a>]: unable to connect to bambooinvoice.org:80 (php_network_getaddresses: getaddrinfo failed: nodename nor servname provided, or not known) /Users/derekallard/Sites/bambooinvoice/bamboo_system_files/application/models/utilities_model.php 12
