c44e0205c153b9258b994a744d70e90a30bbc3aa
Antonio Pérez <aperez@skarcha.com>
2011-03-03 23:49:12 +0100

    Invoice search did not work because of this

--- bamboo_system_files/application/helpers/misc_helper.php
+++ bamboo_system_files/application/helpers/misc_helper.php
@@ -15,9 +15,9 @@ function formatNumber($amount = 0.00, $money = FALSE)
 	$CI =& get_instance();
 
 	$currency_decimal = $CI->config->item('currency_decimal');
+	$currency_thousands = $CI->config->item('currency_thousands');
 
 	if ($money) {
-		$currency_thousands = $CI->config->item('currency_thousands');
 		$currency_symbol = $CI->settings_model->get_setting('currency_symbol');
 
 		if ($CI->settings_model->get_setting('currency_symbol_after') == 'y') {
