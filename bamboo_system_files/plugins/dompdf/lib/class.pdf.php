9a1615a9f4f6f48f8bcff3f6efb802e609f4a526
Frank Bültge <frank@bueltge.de>
2012-02-16 14:47:04 +0100

    fix pdf class for PHP 5.3

--- bamboo_system_files/plugins/dompdf/lib/class.pdf.php
+++ bamboo_system_files/plugins/dompdf/lib/class.pdf.php
@@ -2530,12 +2530,16 @@ class  Cpdf {
           // note that pdf supports only binary format type 1 font files, though there is a
           // simple utility to convert them from pfa to pfb.
           $tmp =  get_magic_quotes_runtime();
-
-          set_magic_quotes_runtime(0);
+          
+          // Check if magic_quotes_runtime is active
+         if ( get_magic_quotes_runtime() )
+             set_magic_quotes_runtime(0);
 
           $data =  file_get_contents($fbfile);
 
-          set_magic_quotes_runtime($tmp);
+          // Check if magic_quotes_runtime is active
+         if ( get_magic_quotes_runtime() )
+             set_magic_quotes_runtime($tmp);
 
 
           // create the font descriptor
@@ -4329,7 +4333,9 @@ class  Cpdf {
 
     $tmp =  get_magic_quotes_runtime();
 
-    set_magic_quotes_runtime(0);
+    // Check if magic_quotes_runtime is active
+    if ( get_magic_quotes_runtime() )
+        set_magic_quotes_runtime(0);
 
     if  ( ($data =  file_get_contents($file)) ===  false) {
 
@@ -4345,7 +4351,9 @@ class  Cpdf {
       $errormsg =  'trouble opening file: '.$file;
     }
 
-    set_magic_quotes_runtime($tmp);
+    // Check if magic_quotes_runtime is active
+    if ( get_magic_quotes_runtime() )
+        set_magic_quotes_runtime($tmp);
 
 
     if  (!$error) {
@@ -4667,14 +4675,20 @@ class  Cpdf {
 
     //$fp = fopen($img,'rb');
 
-    $tmp =  get_magic_quotes_runtime();
+    // Check if magic_quotes_runtime is active
+    if ( get_magic_quotes_runtime() )
+        $tmp =  get_magic_quotes_runtime();
 
-    set_magic_quotes_runtime(0);
+    // Check if magic_quotes_runtime is active
+    if ( get_magic_quotes_runtime() )
+        set_magic_quotes_runtime(0);
 
     $data =  file_get_contents($img);
 
     //fread($fp,filesize($img));
-    set_magic_quotes_runtime($tmp);
+    // Check if magic_quotes_runtime is active
+    if ( get_magic_quotes_runtime() )
+        set_magic_quotes_runtime($tmp);
 
 
     //fclose($fp);
@@ -4740,7 +4754,9 @@ class  Cpdf {
 
     $tmp =  get_magic_quotes_runtime();
 
-    set_magic_quotes_runtime(0);
+    // Check if magic_quotes_runtime is active
+    if ( get_magic_quotes_runtime() )
+        set_magic_quotes_runtime(0);
 
     if  ( ($data =  file_get_contents($tmpName)) ===  false) {
 
@@ -4757,7 +4773,9 @@ class  Cpdf {
     }
 
     //  $data = fread($fp,filesize($tmpName));
-    set_magic_quotes_runtime($tmp);
+    // Check if magic_quotes_runtime is active
+    if ( get_magic_quotes_runtime() )
+        set_magic_quotes_runtime($tmp);
 
     //  fclose($fp);
     unlink($tmpName);
