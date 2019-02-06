23e915abf8d593689fe8fa96ebeb599fe78620b3
Frank Bueltge <frank@bueltge.de>
2019-02-06 10:40:40 +0100

    Fix creation PDF file

--- bamboo_system_files/plugins/to_pdf_pi.php
+++ bamboo_system_files/plugins/to_pdf_pi.php
@@ -3,18 +3,18 @@
 /**
  * Try increasing memory available, mostly for PDF generation
  */
-ini_set("memory_limit","64M");
+ini_set("memory_limit","128M");
+
+require_once 'dompdf/lib/html5lib/Parser.php';
+require_once 'dompdf/lib/php-font-lib/src/FontLib/Autoloader.php';
+require_once 'dompdf/lib/php-svg-lib/src/autoload.php';
+require_once 'dompdf/src/Autoloader.php';
+Dompdf\Autoloader::register();
+
+use Dompdf\Dompdf;
 
 function pdf_create($html, $filename, $stream = true)
 {
-	require_once 'dompdf/lib/html5lib/Parser.php';
-	require_once 'dompdf/lib/php-font-lib/src/FontLib/Autoloader.php';
-	require_once 'dompdf/lib/php-svg-lib/src/autoload.php';
-	require_once 'dompdf/src/Autoloader.php';
-	Dompdf\Autoloader::register();
-	
-	use Dompdf\Dompdf;
-	
 	$dompdf = new Dompdf();
 	$dompdf->set_paper('A4', 'portrait');
 	$dompdf->load_html($html);
@@ -24,19 +24,3 @@ function pdf_create($html, $filename, $stream = true)
 	}
 	write_file("./invoices_temp/$filename.pdf", $dompdf->output());
 }
-
-function old_pdf_create($html, $filename, $stream=TRUE) 
-{
-	require_once(BASEPATH."plugins/dompdf/dompdf_config.inc.php"); 
-//  require_once("dompdf/dompdf_config.inc.php");
-	
-	$dompdf = new DOMPDF();
-	$dompdf->set_paper("a4", "portrait"); 
-	$dompdf->load_html($html);
-	$dompdf->render();
-	if ($stream) {
-		$dompdf->stream($filename.".pdf");
-	}
-	write_file("./invoices_temp/$filename.pdf", $dompdf->output());
-}
-?>
